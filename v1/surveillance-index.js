bd0b1838019f896d715db0510622f0f58332576f
O-drey <98774051+O-drey@users.noreply.github.com>
2022-05-15 19:12:13 +0200

    Add files via upload

---
+++ v1/surveillance-index.js
@@ -0,0 +1,19 @@
+let pointer = document.querySelectorAll(".pointer");
+
+pointer.forEach(function(e){
+    mouseMove(e);
+})
+
+function mouseMove(pointer){
+    document.addEventListener("mousemove",function(e){
+        pointerPos(e, pointer)
+    })
+}
+
+function pointerPos(e, pointer){
+    
+    mousePosX = e.x;
+    mousePosY = e.y;
+    pointer.style.left = mousePosX - (pointer.getBoundingClientRect().width *0.5) + "px";
+    pointer.style.top = mousePosY - (pointer.getBoundingClientRect().height *0.5) + "px";
+}
